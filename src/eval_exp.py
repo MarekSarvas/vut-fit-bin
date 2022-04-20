335818e9ab50598ddd3e094808c0dcfeb4cf91e1
MarekSarvas <sarvas.sarvas@gmail.com>
2022-04-21 00:31:09 +0200

    tables

--- src/eval_exp.py
+++ src/eval_exp.py
@@ -4,11 +4,13 @@
 # Date: 2021/2022                       
 # Module: Boxplot of accuracy of each generation on given dataset.
 
-import matplotlib.pyplot as plt
-import numpy as np
+import os
+import re
 import json
 import argparse
-import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def eval_box(data, path, dataset, title, verbose):
     fig1, ax = plt.subplots(figsize=(10,6))
@@ -27,6 +29,31 @@ def eval_box(data, path, dataset, title, verbose):
     if verbose: plt.show()
 
 
+def eval_table(data, path, dataset):
+
+    path = path.replace(".pdf", ".tex")
+    pd.set_option('display.precision', 4) 
+    df = pd.DataFrame({"min \%": data["min"], "Max \%": data["max"], "Avg \%": data["avg"], "Med \%": data["med"]})
+    df = df.rename_axis('Gen').reset_index()
+    styler = df.style.format({"min \%": '{:.4f}', "Max \%": '{:.4f}', "Avg \%": '{:.4f}', "Med \%": '{:.4f}'})
+
+    styler = styler.hide_index()
+    styler.applymap_index(lambda v: "font-weight: bold;", axis="columns")
+
+
+    tex_content = styler.to_latex(convert_css=True)
+    re_borders = re.compile(r"begin\{tabular\}\{([^\}]+)\}")
+    borders = re_borders.findall(tex_content)[0]
+    borders = '|'.join(list(borders))
+    tex_content = re_borders.sub("begin{tabular}{|" + borders + "|}", tex_content)
+    tex_content = tex_content.replace("\\bfseries", "\hline\n\\bfseries", 1) # add upper border
+    tex_content = tex_content.replace("0", "\hline\n 0", 1) # add first row border
+    tex_content = tex_content.replace("\\end", "\hline\n\\end", 1) # add first row border
+    tex_content = tex_content.replace("Med", "\\bfseries Med", 1) # add first row border
+
+    with open(path, "w") as f:
+        f.write(tex_content)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, default="mnist")
@@ -44,7 +71,7 @@ if __name__ == '__main__':
             title = (path.rsplit("/", 1)[-1]+name).replace(".json", "").replace("exp_", "")
             
             # store data for plotting
-            data = {"generation": np.array([], dtype=object), "fitness": [], "genotype": np.array([], dtype=object)}
+            data = {"generation": np.array([], dtype=object), "fitness": [], "genotype": np.array([], dtype=object), "avg": [], "min": [], "max": [], "med": []}
 
             with open(json_path) as f:
                 json_data = json.load(f)
@@ -62,6 +89,11 @@ if __name__ == '__main__':
                 # store genotype with highest fitness in current generation
                 data["genotype"] = np.append(data["genotype"], item[1][np.argmax(tmp)]["genotype"])
                 data["fitness"].append(np.array(tmp))
+                data["min"].append(np.min(tmp))
+                data["max"].append(np.max(tmp))
+                data["avg"].append(np.mean(tmp))
+                data["med"].append(np.median(tmp))
 
             # plot data
             eval_box(data, plot_path, params.dataset, title, params.verbose) 
+            eval_table(data, plot_path, params.dataset)
