19ea39148588da7231e88966570f35facc0982db
Marek Sarvas <xsarva00@perian.ncbr.muni.cz>
2022-05-05 23:10:56 +0200

    meta config

---
+++ run_meta.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/bash
+
+
+stage=2
+stop_stage=2
+dumpdir=dump
+verbose=0
+
+baseline=cnn
+
+epochs=5
+p_m=0.8
+q_m=0.1
+p_c=0.2
+q_c=0.3
+stages=3
+nodes="3_4_5"
+dataset="cifar10"
+population_size=20
+generations=30
+
+
+exp_id=exp_stages${stages}_nodes${nodes}
+tag="pop_${population_size}_gen_${generations}_epochs_${epochs}_pm${p_m}_qm${q_m}_pc${p_c}_qc${q_c}"
+
+
+EXP_PATH=./exp_meta/${dataset}/${exp_id}
+
+mkdir -pv ${EXP_PATH}
+
+if [ ${stage} -le 0 ] &&[ ${stop_stage} -ge 0 ]; then
+    echo "stage 0: Data Preparation"
+    rm -r ../data/MNIST/
+    python download_data.py
+fi
+
+
+if [ ${stage} -le 1 ] &&[ ${stop_stage} -ge 1 ]; then
+    echo "stage 1: Baseline Model Training"
+    if [ -z ${tag} ]; then
+        expname=mnist_${baseline}_epochs_${epochs} 
+    else
+        expname=mnist_${baseline}_${tag} 
+    fi
+    mkdir -pv ../exp/${expname}
+    python train_eval.py
+fi
+
+if [ ${stage} -le 2 ] &&[ ${stop_stage} -ge 2 ]; then
+    echo "stage 2: Neuroevolution"
+    python3 evolution.py  --generations ${generations} \
+                        --population_size ${population_size} \
+                        --epochs ${epochs} \
+                        --exp_path ${EXP_PATH}/${tag}.json \
+                        --pm ${p_m} \
+                        --qm ${q_m} \
+                        --pc ${p_c} \
+                        --qc ${q_c} \
+                        --cnn_stages ${stages} \
+                        --cnn_nodes ${nodes} \
+                        --gpu 1 \
+                        --dataset ${dataset} \
+                        --verbose True \
+                #        > ${EXP_PATH}/${tag}.log 
+fi
+
---
+++ train_job.sh
@@ -0,0 +1,47 @@
+#!/bin/bash    
+#PBS -N bin_exp
+#PBS -l select=1:ncpus=2:mem=16gb:scratch_local=50gb:ngpus=1:cl_adan=True    
+#PBS -q gpu    
+#PBS -l walltime=10:00:00    
+#PBS -m ae
+
+
+# define BASE TODO: alter..
+export BASE=/storage/brno2/home/xsarva00/
+
+# append a line to a file "jobs_info.txt" containing the ID of the job, the hostname of node it is run on and the path to a scratch directory
+# this information helps to find a scratch directory in case the job fails and you need to remove the scratch directory manually     
+echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $DATADIR/jobs_info.txt    
+     
+# test if scratch directory is set    
+# if scratch directory is not set, issue error message and exit    
+test -n "$SCRATCHDIR" || { echo >&2 "Variable SCRATCHDIR is not set!"; exit 1; }    
+
+
+# load modules    
+module add anaconda3-4.0.0    
+module add conda-modules-py37    
+    
+# activate (or create) the conda environment    
+conda activate torch_env || {    
+  conda create -n torch_env python=3.7;    
+  conda activate torch_env;    
+  conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch;
+  conda install scipy;    
+  conda install -c conda-forge matplotlib;
+}
+
+mkdir $SCRATCHDIR/data     
+    
+# move into scratch directory
+cd $SCRATCHDIR
+
+# copy the repository..
+cp -r $BASE/vut-fit-bin .
+cd vut-fit-bin
+
+# run the training..
+bash run_meta.sh
+
+# clean the SCRATCH directory and exit :)
+clean_scratch
